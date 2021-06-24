1eab9b7821b2a66475929e40dc67738f7bf59c3a
Chris Wood <cawoodlfc@hotmail.co.uk>
2021-06-24 14:45:53 +0100

    deployment

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 import router from './views/router.js'
 import logger from './middleware/logger.js'
 import errorHandler from './middleware/errorHandler.js'
@@ -6,6 +7,7 @@ import errorHandler from './middleware/errorHandler.js'
 const app = express()
 // ! Order is important for these
 app.use(express.json())
+app.use(cors())
 app.use(logger)
 app.use('/api', router)
 app.use(errorHandler)
--- config/environment.js
+++ config/environment.js
@@ -1,3 +1,7 @@
-export const dbURL = 'mongodb://localhost/R8MyPl8'
+import dotenv from 'dotenv'
+dotenv.config()
 
-export const secret = 'TheMeatFootIsTheTastiestOutOfThemAll'
+
+export const dbURL = process.env.DB_URI || 'mongodb://localhost/R8MyPl8'
+export const port = process.env.PORT || 4000
+export const secret = process.env.SECRET || 'shhh its a secret'
